5bd274855b09ead1bb1f154c2a8efcf7c837850b
ChukwuEmeka1269 <emekaukwuoma546@gmail.com>
2022-12-04 09:35:43 +0100

    Added handlers for adding a note session and destroying a session

--- src/main/java/com/emekaukwuoma/SpringBootSessionManagement/controller/SessionController.java
+++ src/main/java/com/emekaukwuoma/SpringBootSessionManagement/controller/SessionController.java
@@ -2,7 +2,10 @@ package com.emekaukwuoma.SpringBootSessionManagement.controller;
 
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -22,6 +25,19 @@ public class SessionController {
         }
         note_session.add(note);
         setSessionAttritube(request, note_session);
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model, HttpSession session){
+        List<String> notes = (List<String>) session.getAttribute("NOTE_SESSION");
+        model.addAttribute("noteSession", Objects.nonNull(notes) ? notes:new ArrayList<>());
+        return "home";
+    }
+
+    @PostMapping("/invalidate/session")
+    public String destroySession(HttpServletRequest request){
+        request.getSession().invalidate();
         return "redirect:/home";
     }
 
